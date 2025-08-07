0bb6d824f1132c2c890eed9372520815388433f6
dmerkurjev <36778179+dmerkurjev@users.noreply.github.com>
2025-08-07 16:19:10 -0700

    Update Code_to_Answers.sh

--- workspace/Code_to_Answers.sh
+++ workspace/Code_to_Answers.sh
@@ -9,12 +9,12 @@ Answers to questions path, please run full script succesfully first to run those
 sample_lanes <- c(EndoC-βH1_control = 1L, CVB4-E2_in = 1L, EndoC-βH1_CVB4-JVB_in = 1L, Sw.71_control = 1L, Sw.71_CVB4-JVB_in = 1L)
 ans_q1 <- unname(sample_lanes["ym"])
 
-# q2: What is the library size (total read counts) for siRNA_NCI-H460_NT_Rep1?
+# q2: What is the library size (total read counts) for Sw.71_control?
 libsize <- colSums(counts)
-ans_q2 <- unname(libsize["H460_L1"])+ unname(libsize["H460_L2"])
+ans_q2 <- unname(libsize["Sw.71_control"]))
 
-# q3: How many genes have nonzero counts in sample EndoC-βH1_CVB4-JVB infected?
-ans_q3 <- sum(counts[, "ym"] > 0)
+# q3: How many genes have nonzero counts in sample EndoC-βH1_CVB4-JVB_infected?
+ans_q3 <- sum(counts[, "EndoC-βH1_CVB4-JVB_infected"] > 0)
 
 # q4: How many genes are upregulated \uc0\u8805 2-fold (log2FC \u8805  1) in EndoC-βH1_control vs. EndoC-βH1_CVB4-JVB_infected with FDR < 0.01?
 res_df <- as.data.frame(res)
@@ -25,7 +25,7 @@ res_df$SYMBOL <- rownames(res_df)
 up_rank <- res_df %>%
   dplyr::filter(!is.na(log2FoldChange) & log2FoldChange > 0) %>%
   dplyr::arrange(dplyr::desc(log2FoldChange), padj, pvalue)
-ans_q5 <- if (nrow(up_rank) >= 4) up_rank$SYMBOL[3] else NA_character_
+ans_q5 <- if (nrow(up_rank) >= 7) up_rank$SYMBOL[3] else NA_character_
 
 answers <- tibble::tibble(
   id = c("q1","q2","q3","q4","q5"),
