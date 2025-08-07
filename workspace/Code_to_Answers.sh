ade47da765e2fa907a405b4c3d6dfcfbd01b4bad
dmerkurjev <36778179+dmerkurjev@users.noreply.github.com>
2025-08-07 15:44:50 -0700

    Update Code_to_Answers.sh

--- workspace/Code_to_Answers.sh
+++ workspace/Code_to_Answers.sh
@@ -4,7 +4,7 @@ Answers to questions path, please run full script succesfully first to run those
 # ---- Answers for q1..q5 ----
 # Requires objects 'counts', 'dds', and 'res' created above
 
-# q1: How many sequencing lanes were concatenated to form sample siRNA_ECC-1_NT_Rep1
+# q1: "How many sequencing lanes were concatenated to form sample EndoC-βH1_control?
 # If you later generate a manifest upstream, read it here instead of hardcoding
 sample_lanes <- c(ECC1_L1 = 4L, ECC1_L2 = 4L, H460_L1 = 4L, H460_L2 = 4L, ECC1_L1 = 4L, ECC1_L2 = 4L, PC-3_L1= 4L, PC-3_L2= 4L, PC_3_L1_siD= 4L, PC_3_L2_sid= 4L)
 ans_q1 <- unname(sample_lanes["ym"])
@@ -13,14 +13,14 @@ ans_q1 <- unname(sample_lanes["ym"])
 libsize <- colSums(counts)
 ans_q2 <- unname(libsize["H460_L1"])+ unname(libsize["H460_L2"])
 
-# q3: How many genes have nonzero counts in sample siRNA_PC-3_siDICER1_Rep1_L1?
+# q3: How many genes have nonzero counts in sample EndoC-βH1_CVB4-JVB infected?
 ans_q3 <- sum(counts[, "ym"] > 0)
 
-# q4: How many genes are upregulated \uc0\u8805 2-fold (log2FC \u8805  1) in siRNA_ECC-1_NT_Rep1 vs. siRNA_PC-3_siDICER1_Rep1_L1 with FDR < 0.01?
+# q4: How many genes are upregulated \uc0\u8805 2-fold (log2FC \u8805  1) in EndoC-βH1_control vs. EndoC-βH1_CVB4-JVB_infected with FDR < 0.01?"
 res_df <- as.data.frame(res)
 ans_q4 <- sum(res_df$log2FoldChange >= 1 & res_df$padj < 0.01, na.rm = TRUE)
 
-# q5: Which gene is ranked 4th by log2 fold change (most upregulated) in siRNA_ECC-1_NT_Rep1 vs. siRNA_PC-3_siDICER1_Rep1_L1?
+# q5: Which gene is ranked 7th by log2 fold change (most upregulated) in EndoC-βH1_control vs. EndoC-βH1_CVB4-JVB_infected?
 res_df$SYMBOL <- rownames(res_df)
 up_rank <- res_df %>%
   dplyr::filter(!is.na(log2FoldChange) & log2FoldChange > 0) %>%
