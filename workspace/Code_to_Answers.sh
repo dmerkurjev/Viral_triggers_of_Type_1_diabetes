89720a1d978ea985554bc5e9947e5aa79d91ea4b
dmerkurjev <36778179+dmerkurjev@users.noreply.github.com>
2025-08-08 07:50:46 -0700

    Update Code_to_Answers.sh

--- workspace/Code_to_Answers.sh
+++ workspace/Code_to_Answers.sh
@@ -16,16 +16,16 @@ ans_q2 <- unname(libsize["Sw.71_control"]))
 # q3: How many genes have nonzero counts in sample EndoC-βH1_CVB4-JVB_infected?
 ans_q3 <- sum(counts[, "EndoC-βH1_CVB4-JVB_infected"] > 0)
 
-# q4: How many genes are upregulated \uc0\u8805 2-fold (log2FC \u8805  1) in EndoC-βH1_control vs. EndoC-βH1_CVB4-JVB_infected with FDR < 0.01?
+# q4: How many genes are upregulated or downregulated \uc0\u8805 2-fold (log2FC \u8805  1) in EndoC-βH1_control vs. EndoC-βH1_CVB4-JVB_infected with FDR < 0.01?
 res_df <- as.data.frame(res)
-ans_q4 <- sum(res_df$log2FoldChange >= 1 & res_df$padj < 0.01, na.rm = TRUE)
+ans_q4 <- sum(res_df$log2FoldChange >= 1 & res_df$padj < 0.05, na.rm = TRUE)
 
-# q5: Which gene is ranked 7th by log2 fold change (most upregulated) in EndoC-βH1_control vs. EndoC-βH1_CVB4-JVB_infected?
+# q5: Which gene is ranked 2th by log2 fold change (upregulated) in EndoC-βH1_control vs. EndoC-βH1_CVB4-JVB_infected?
 res_df$SYMBOL <- rownames(res_df)
 up_rank <- res_df %>%
   dplyr::filter(!is.na(log2FoldChange) & log2FoldChange > 0) %>%
   dplyr::arrange(dplyr::desc(log2FoldChange), padj, pvalue)
-ans_q5 <- if (nrow(up_rank) >= 7) up_rank$SYMBOL[3] else NA_character_
+ans_q5 <- if (nrow(up_rank) >= 2) up_rank$SYMBOL[3] else NA_character_
 
 answers <- tibble::tibble(
   id = c("q1","q2","q3","q4","q5"),
