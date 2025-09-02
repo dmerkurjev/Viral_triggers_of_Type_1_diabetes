e0be07aa156dbd0dd99f23a1396a85821890580f
dmerkurjev <36778179+dmerkurjev@users.noreply.github.com>
2025-09-02 15:13:22 -0700

    Update Code_to_Answers.sh

--- workspace/Code_to_Answers.sh
+++ workspace/Code_to_Answers.sh
@@ -4,16 +4,16 @@ Answers to questions path, please run full script succesfully first to run those
 # ---- Answers for q1..q5 ----
 # Requires objects 'counts', 'dds', and 'res' created above
 
-# q1: "How many sequencing lanes were concatenated to form sample EndoC-βH1_control_rep1?
+# q1: How many duplicate reads were in the sample EndoC-βH1_control_rep1?
 # If you later generate a manifest upstream, read it here instead of hardcoding
 sample_lanes <- c(EndoC-βH1_control_rep1 = 1L, EndoC-βH1_control_rep2 = 1L, CVB4-E2_in_rep1 = 1L, CVB4-E2_in_rep2 = 1L, EndoC-βH1_CVB4-JVB_in_rep1 = 1L, EndoC-βH1_CVB4-JVB_in_rep2 = 1L)
 ans_q1 <- unname(sample_lanes["EndoC-βH1_control_rep1"])
 
-# q2: What is the library size (total read counts) for EndoC-βH1_control_rep2?
+# q2: What is the library size (total read counts) is higher: EndoC-βH1_control_rep2 or EndoC-βH1_CVB4-JVB_infected?
 libsize <- colSums(counts)
 ans_q2 <- unname(libsize["EndoC-βH1_control_rep2"]))
 
-# q3: How many genes have nonzero counts in sample EndoC-βH1_CVB4-JVB_infected?
+# q3: How many genes have counts >1 in sample EndoC-βH1_CVB4-JVB_infected_rep1 or EndoC-βH1_CVB4-JVB_infected_rep2?
 ans_q3 <- sum(counts[, "EndoC-βH1_CVB4-JVB_infected"] > 0)
 
 # q4: How many genes are upregulated or downregulated \uc0\u8805 2-fold (log2FC \u8805  1) in EndoC-βH1_control vs. EndoC-βH1_CVB4-JVB_infected with FDR < 0.01?
